8f509553f39867b1f8e721224702a3b101bc6069
amsuarezp <amsuarezp@gmail.com>
2020-10-03 20:09:29 -0500

    service code

---
+++ RedesL3A1Server/src/servidor/ProtocoloServidor.java
@@ -0,0 +1,167 @@
+package servidor;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.security.DigestInputStream;
+import java.security.KeyPair;
+import java.security.MessageDigest;
+import java.security.cert.X509Certificate;
+
+public class ProtocoloServidor implements Runnable{
+	
+	//Rutas de archivos
+	private static String ARCHIVO_1 = "data/archivos/ElAñañin.mp4";
+	private static String ARCHIVO_2 = "data/archivos/toadload.wav";
+	
+	//Constantes
+	private static int TAM_PAQUETE = 1024;
+
+	//Atributos
+	private Socket sc = null;
+	private int idP;
+	private long time_start, time_end, time;
+	private static File fileLog;
+	private int archivo;
+
+
+	/*
+	 * Constructor del protocolo del servidor
+	 * @param: csP socket designado
+	 * @param: idP Numero de thread que atiende
+	 */
+	public ProtocoloServidor (Socket csP, int idP, int numArchivo, File archivoLog) {
+		
+		fileLog = archivoLog;
+		sc = csP;
+		this.idP=idP;
+		archivo = numArchivo;
+		this.run();
+
+	}
+
+	/*
+	 * Generacion del archivo log. 
+	 * Nota: 
+	 * - Debe conservar el metodo . 
+	 * - Es el ÃƒÂºnico metodo permitido para escribir en el log.
+	 */
+	private void escribirLog(String pCadena) {
+		synchronized(fileLog)
+		{
+			try {
+				FileWriter fw = new FileWriter(fileLog,true);
+				fw.append(pCadena + "\n");
+				fw.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+	
+	/**
+	 * Método que genera hash para verificar inttegridad
+	 * @param ruta ruta del archivo
+	 * @param md MessageDigest que iene el algoritmo
+	 * @return String hexadecimal con hash de archivo
+	 * @throws IOException si falla lectura de archivo
+	 */
+	private String checksum(String ruta, MessageDigest md ) throws IOException{
+		try (DigestInputStream dis = new DigestInputStream(new FileInputStream(ruta), md)) {
+            while (dis.read() != -1) ; //empty loop to clear the data
+            md = dis.getMessageDigest();
+        }
+
+        // bytes to hex
+        StringBuilder result = new StringBuilder();
+        for (byte b : md.digest()) {
+            result.append(String.format("%02x", b));
+        }
+        return result.toString();
+	}
+	
+	@Override
+	public void run() {
+
+		try {
+			
+			//RECUPERA EL ARCHIVO A ENVIAR
+			File file;
+			if(archivo == 1) {
+				file = new File(ARCHIVO_1);
+			}
+			else {
+				file = new File(ARCHIVO_2);
+			}
+			//CREA BUFFER Y CANALES DE COMUNICACION EN SOCKET
+			byte[] archivoBytes = new byte[(int) file.length()];
+			FileInputStream fi = new FileInputStream(file);
+			BufferedInputStream bis = new BufferedInputStream(fi);
+			bis.read(archivoBytes, 0, archivoBytes.length);
+			OutputStream os = sc.getOutputStream();
+			
+			//GENERA HASH DEL ARCHIVO PARA COMPROBACION
+			
+			//VERSION PRUEBA
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			String hexa = checksum(file.getPath(), md);
+			//System.out.println("Hexa serv " + hexa);
+			DataOutputStream dos = new DataOutputStream(sc.getOutputStream());
+			dos.writeUTF(hexa);
+			dos.writeInt((int)file.length());
+			//NOTIFICA ENVIO DE ARCHIVO Y COMIENZA PROCESO
+			System.out.println("Enviando "+ file.getName() + " tamano: " + archivoBytes.length + " Bytes");
+			
+			int enviados = 0;
+			time_start = System.currentTimeMillis();
+			for(int i = 0; i <= file.length() - (file.length() % TAM_PAQUETE) ; i+=TAM_PAQUETE ) {
+				if(i == file.length() - (file.length() % TAM_PAQUETE)) {
+					os.write(archivoBytes, i, (int)file.length() % TAM_PAQUETE);
+				}
+				else {
+					os.write(archivoBytes, i, TAM_PAQUETE);
+				}
+				enviados++;
+			}
+			
+			
+			//NOTIFICA TERMINACION DE ENVIO Y RECEPCION DEL ARCHIVO POR PARTE EL CLIENTE
+			DataInputStream dis = new DataInputStream(sc.getInputStream());
+			if(dis.readByte() == 1) {
+				time_end = System.currentTimeMillis();
+				time = time_end - time_start;
+				String cadena = "Entrega de archivo a cliente " + idP + " fue exitosa. Tomó " + time / 1000 + " segundos";
+				escribirLog(cadena);
+				cadena = "Paquetes enviados " + enviados + " se enviaron " + (int) file.length() + " Bytes";
+				escribirLog(cadena);
+				cadena = "Paquetes recibidos " + enviados + " se recibieron " + (int) file.length() + " Bytes";
+				escribirLog(cadena);
+				System.out.println("Envío de archivo terminado. Cliente ya lo recibió.");
+			}
+			dis.close();
+			bis.close();
+			os.close();
+			sc.close();
+		}
+		catch(Exception e) {
+			System.out.println("Error en proceso de envío... " + e.getMessage());
+		}
+		
+	}
+
+
+
+}
---
+++ RedesL3A1Server/src/servidor/mainServ.java
@@ -0,0 +1,141 @@
+package servidor;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import servidor.ProtocoloServidor;
+
+
+public class mainServ {
+	private static ServerSocket ss;	
+	private static final String MAESTRO = "MAESTRO: ";
+	public static int contInst;
+	private static final String ARCHIVO_1 = "video1.mp4";
+	private static final String ARCHIVO_2 = "test2.bin";
+	private static final String RUTA_LOG = "data/log/";
+	private static final String RUTA_ARCH = "data/archivos/";
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception{
+
+		
+		System.out.println(MAESTRO + "Establezca puerto de conexion:");
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(isr);
+		int ip = Integer.parseInt(br.readLine());
+		System.out.println(MAESTRO + "Empezando servidor maestro en puerto " + ip);
+		
+		System.out.println(MAESTRO + "Escribe el número del archivo a transmitir: 1 para el archivo de 100MiB o 2 para el archio de 250MiB");
+		int archivo = Integer.parseInt(br.readLine());
+		
+		// Crea el archivo de log
+		
+		File archivoLog = null;
+		
+		String time = new SimpleDateFormat("dd_MM_yyyy_HH_mm_ss").format(Calendar.getInstance().getTime());
+		archivoLog = new File(RUTA_LOG + time + ".txt");
+		escribirLog(archivoLog, "Fecha y hora de prueba " + time);
+		File archivoEnviar;
+		if(archivo == 1) {
+			archivoEnviar = new File(RUTA_ARCH+ARCHIVO_1);
+			escribirLog(archivoLog, "Nombre del archivo a enviar: " + ARCHIVO_1 + " de tamaño " + (int)archivoEnviar.length() + " Bytes");
+			
+		}
+		else {
+			archivoEnviar = new File(RUTA_ARCH+ARCHIVO_2);
+			escribirLog(archivoLog, "Nombre del archivo a enviar: " + ARCHIVO_2 + " de tamaño " + (int)archivoEnviar.length() + " Bytes");
+		}
+		
+		
+		// Crea el socket que escucha en el puerto seleccionado.
+		ss = new ServerSocket(ip);
+		System.out.println("Por favor introduzca el número máximo de cientes que quiere atender (no más de 25) ");
+		int nThreads = Integer.parseInt(br.readLine());
+
+		Socket[] socketClientes = new Socket[nThreads];
+		
+		System.out.println("Esperando conexiones...");
+		
+		int conectados = 0;
+		while(conectados < nThreads) {
+			try {
+				//INICIA PROTOCOLO ACEPTANDO CONEXION
+				socketClientes[conectados] = ss.accept();
+				DataOutputStream dout = new DataOutputStream(socketClientes[conectados].getOutputStream());
+				DataInputStream din = new DataInputStream(socketClientes[conectados].getInputStream());
+				System.out.println("Aceptando conexión de cliente numero: " + conectados);
+				
+				//TRAS ADMITIR CLIENTE Y CREAR COMUNICACION ENTRADA-SALIDA EN SOCKET SE ENVÍA EL ID AL CLIENTE				
+				dout.writeByte(0);
+				
+				//SE NOTIFICA EL NOMBRE DEL ARCHIVO QUE SE ENVIARÁ
+				if(archivo == 1) {
+					dout.writeInt(conectados);
+					dout.writeUTF(ARCHIVO_1);
+					System.out.println("Escogio archivo 1");
+				}
+				else {
+					dout.writeInt(conectados);
+					dout.writeUTF(ARCHIVO_2);
+					dout.flush();
+					System.out.println("Escogio archivo 2");
+
+				}
+				
+				//SE PREPARA RECEPCION DE SIGUIENTE CONEXION Y SE NOTIFICA QUE CLIENTE ESPERA
+				if(din.readUTF().contentEquals("OK")) {
+					System.out.println("Cliente "+ conectados + " recibió ID y nombre de archivo");
+				}
+				//SE PREPARA LLEGADA DE SIGUIENTE CLIENTE
+				conectados++;
+			
+			}
+			catch(Exception e) {
+				e.printStackTrace();
+				System.out.println("Error en servidor! " );
+			}
+		}
+		//ACABA WHILE, ES DECIR, YA LLEGARON CLIENTES ESPERADOS Y SE EMPIEZA ENVÍO DE ARCHIVO
+		System.out.println("Comenzando envío de archivo a clientes...");
+		//RECORRE N CLIENTES Y ENVÍA ARCHIVO
+		for(int cli=0; cli<socketClientes.length;cli++) {
+			escribirLog(archivoLog, "Enviando a cliente de ID " + cli);
+			ProtocoloServidor ps = new ProtocoloServidor(socketClientes[cli], cli, archivo, archivoLog);
+			
+		}
+		
+
+	}
+	/*
+	 * Generacion del archivo log. 
+	 * Nota: 
+	 * - Debe conservar el metodo . 
+	 * - Es el ÃƒÂºnico metodo permitido para escribir en el log.
+	 */
+	public static void escribirLog(File fileLog, String pCadena) {
+				{
+			try {
+				FileWriter fw = new FileWriter(fileLog,true);
+				fw.append(pCadena + "\n");
+				fw.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+ 
+}
